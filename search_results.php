b1307b1238a6db3d46b439196d01720b7d619912
Chisato <49408516+choco999@users.noreply.github.com>
2021-03-07 16:18:46 -0500

    added search functionality

---
+++ search_results.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Birthday Wish Item Forum</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+</head>
+<body>
+
+<?php 
+    //session_start();
+
+    echo "<div class='container'>";
+
+    $submit = filter_input(INPUT_GET, 'submit');
+    $search_term = filter_input(INPUT_GET, 'search');
+    $name = filter_input(INPUT_GET, 'name');
+
+    require('connect.php');
+
+    //$_SESSION['name'] = $name;
+
+    $sql = "SELECT title, birthday_item from course_project 
+            WHERE title LIKE :search_term OR birthday_item LIKE :search_term;";
+    
+    $statement = $db->prepare($sql);
+
+    $statement->bindValue(':search_term', '%' .$search_term . '%');
+
+    $statement->execute();
+
+    echo "<table><tbody>";
+
+    if(!empty($search_term)){
+        if($statement->rowCount() >= 1){
+            echo "<h1> We found " . $statement->rowCount() . " items related to '" . $search_term. "'</h1>";
+            while($results = $statement->fetch()){
+                echo "<tr><td>" .$results['title'] . "</td><td>" 
+                                .$results['birthday_item'] . "</td></tr>";
+            }
+        }
+        else {
+            echo "<h1>No items found related to ". $search_term. "</h1>";
+        }
+    }
+    else {
+        echo"<h1>" . $name . " You didn't enter any search terms.</h1>";
+    }
+
+    echo "</tbody></table>";
+
+    
+    echo "<a href='view.php'>Back to Search</a>";
+    echo "</div>";
+
+    $statement->closeCursor();
+?>
--- view.php
+++ view.php
@@ -20,7 +20,7 @@
                         .$record['age'] . "</td><td>" 
                         .$record['gender'] . "</td><td>" 
                         .$record['birthday_item'] . "</td><td><a href='delete.php?id=" . $record['id'] ."'>Delete</a></td>
-                        <td><a href='index.php?id=" . $record['id'] . "'>Edit</a><td></tr>";
+                        <td><a href='index.php?id=" . $record['id'] . "'>Edit</a></td></tr>";
     }
 
     echo "</tbody></table>";
@@ -28,5 +28,18 @@
     $statement->closeCursor();
 
     ?>
+
+<h2>Search Birthday Wish Items Here</h2>
+<form action="search_results.php" method="get">
+    <div class="row">
+        <div class="col">
+            <input type ="text" name="name" placeholder="Your name" class="form-control">
+        </div>
+        <div class="col">
+            <input type="text" name="search" placeholder="Search for a birthday item" class="form-control">
+        </div>
+        <input type="submit" name="submit" value="Search" class="btn btn-primary">
+    </div>
+</form>
 
 <?php require('footer.php'); ?>

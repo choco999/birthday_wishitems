22b166b75390b964b0aafedbe453afecef29c262
Chisato <49408516+choco999@users.noreply.github.com>
2021-04-05 22:08:06 -0400

    Added recaptcha

---
+++ config.php
@@ -0,0 +1,4 @@
+<?php
+
+  define('SITEKEY', '6Lc7DpcaAAAAAIyOrqtGVv0mDBYiG-4em3Ne78i2');
+  define('SECRETKEY','6Lc7DpcaAAAAABOi9uAq-xruIYiQKO8QvDJdpY-m');
--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php require('header.php');
+    if (session_status() === PHP_SESSION_NONE) {
+        session_start();
+    }
+    
+    $form_values = $_SESSION['form_values'] ?? null;
+
+    unset($_SESSION['form_values']);
+
     $id = null;
     $title = null;
     $author = null;
@@ -35,15 +43,15 @@
     }
 ?>
     <main>
-        <form action="process.php" method="post">
+        <form action="process.php" method="post" novalidate>
             <input type="hidden" name="id" value="<?php echo $id?>">
 
             <label for="title">Title</label>
-            <input type="text" name="title" id="title" class="form-control" value="<?php echo $title?>" required>
+            <input type="text" name="title" id="title" class="form-control" value="<?= $form_values['title'] ?? null ?>" required>
             <label for="author">Author</label>
-            <input type="text" name="author" id="author" class="form-control" value="<?php echo $author?>" required>
+            <input type="text" name="author" id="author" class="form-control" value="<?= $form_values['author'] ?? null ?>" required>
             <label for="age">Age</label>
-            <input type="number" name="age" id="age" class="form-control" value="<?php echo $age?>" required>
+            <input type="number" name="age" id="age" class="form-control" value="<?= $form_values['age'] ?? null ?>" required max="120" min="0" step="1">
 
             <label for="gender">Gender</label>
             <div>
@@ -56,8 +64,10 @@
             </div>
 
             <label for="bday-item">What do you want for your birthday?</label>
-            <textarea name="bday-item" id="bday-item" cols="30" rows="10" class="form-control" required><?php echo $birthday_item?></textarea>
+            <textarea name="bday-item" id="bday-item" cols="30" rows="10" class="form-control" required><?= $form_values['bday-item'] ?? null ?></textarea>
             <input type="submit" value="submit" name="submit">
+        <!-- Add the recaptcha field -->
+        <input type="hidden" name="recaptcha_response" id="recaptchaResponse">
         </form>
     </main>
     <?php require('footer.php');?>
